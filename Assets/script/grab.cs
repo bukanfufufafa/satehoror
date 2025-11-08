9fcb93501745f904423584ec6b71c03d9d1b0a01
bukanfufufafa <nabilrizky249@gmail.com>
2025-11-08 23:15:28 +0700

    character movement

---
+++ Assets/script/grab.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class grab : MonoBehaviour
+{
+    public GameObject Objectinrange;
+    public GameObject ObjectGrabbed;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/script/playermovement.cs
+++ Assets/script/playermovement.cs
@@ -1,16 +1,37 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class playermovement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMove2D : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f; // Kecepatan gerak
+    
+    private Rigidbody2D rb;
+    private float moveInputY;
+    private float moveInputX;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Ambil input horizontal (A/D atau panah kiri/kanan)
+        moveInputX = Input.GetAxisRaw("Horizontal");
+        moveInputY = Input.GetAxisRaw("Vertical");
+        // Balik arah sprite jika perlu
+
     }
+
+    void FixedUpdate()
+    {
+        // Gerakkan karakter ke kiri/kanan
+        rb.linearVelocity = new Vector2(moveInputX * moveSpeed, moveInputY * moveSpeed);
+    }
+
+    
+
+
 }
