29098f42a88ab7e069111a7f7cbc22a899208e8d
bukanfufufafa <nabilrizky249@gmail.com>
2025-11-08 23:48:33 +0700

    cameracontroller

--- Assets/script/playermovement.cs
+++ Assets/script/playermovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMove2D : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Kecepatan gerak
+    public float moveSpeed = 5f; 
     
     private Rigidbody2D rb;
     private float moveInputY;
@@ -18,16 +18,16 @@ public class PlayerMove2D : MonoBehaviour
 
     void Update()
     {
-        // Ambil input horizontal (A/D atau panah kiri/kanan)
+        
         moveInputX = Input.GetAxisRaw("Horizontal");
         moveInputY = Input.GetAxisRaw("Vertical");
-        // Balik arah sprite jika perlu
+        
 
     }
 
     void FixedUpdate()
     {
-        // Gerakkan karakter ke kiri/kanan
+        
         rb.linearVelocity = new Vector2(moveInputX * moveSpeed, moveInputY * moveSpeed);
     }
 
